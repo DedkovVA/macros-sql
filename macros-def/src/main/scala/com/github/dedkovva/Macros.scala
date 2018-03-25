0f76d34773af9b60f55f0bcc4d5c4c0c61db6ef0
sbt-dedkov-va <vadedkov.sbt@sberbank.ru>
2018-03-25 12:40:03 +0300

    with simple where clause

--- macros-def/src/main/scala/com/github/dedkovva/Macros.scala
+++ macros-def/src/main/scala/com/github/dedkovva/Macros.scala
@@ -13,81 +13,209 @@ object Macros {
   def selectImpl(c: blackbox.Context)(f: c.Tree): c.Expr[String] = {
     import c.universe._
 
-//    Apply(
-//      Select(
-//        This(TypeName("SqlExample")),
-//        TermName("select")
-//      ),
-//      List(
-//        Function(
-//          List(ValDef(Modifiers(PARAM), TermName("t"), TypeTree().setOriginal(Ident(com.github.dedkovva.TABLE_1)), EmptyTree)),
-//          Apply(
-//            TypeApply(
-//              Select(Select(Ident(scala), scala.Tuple2), TermName("apply")),
-//              List(TypeTree(), TypeTree())
-//            ),
-//            List(Select(Ident(TermName("t")), TermName("COLUMN_A")), Select(Ident(TermName("t")), TermName("COLUMN_B")))
-//          )
-//        )
-//      )
-//    )
-
-    //SqlExample.this.select(((t: com.github.dedkovva.TABLE_1) => scala.Tuple2.apply[String, String](t.COLUMN_A, t.COLUMN_B)))
-//    val Function(Function(List(ValDef(_, name, _, _)), _)) = f
-
-//    c.Expr(q"$name")
-
-//    val Apply(Select(_, TermName("select")), List(Function(List(ValDef(_, _, tpt, _)), _))) = f
-//    val Ident(TermName(tableName)) = tpt
-
-//    Function(
-//      List(ValDef(Modifiers(PARAM), TermName("t"), TypeTree().setOriginal(Ident(com.github.dedkovva.TABLE_1)), EmptyTree)),
-//      Apply(
-//        TypeApply(Select(Select(Ident(scala), scala.Tuple2), TermName("apply")), List(TypeTree(), TypeTree())),
-//        List(Select(Ident(TermName("t")), TermName("COLUMN_A")), Select(Ident(TermName("t")), TermName("COLUMN_B")))
-//      )
-//    )
-
-//    Function(
-//      List(ValDef(Modifiers(PARAM), TermName("t"), TypeTree().setOriginal(Ident(com.github.dedkovva.TABLE_1)), EmptyTree)),
-//      Select(Ident(TermName("t")), TermName("COLUMN_A"))
-//    )
-
-    val Function(List(ValDef(_, _, tpt, EmptyTree)), columnsSelector) = f
+    val Function(
+      List(ValDef(_, _, tpt, EmptyTree)),
+      Apply(
+        Select(Ident(TermName("SqlSelect")), TermName("apply")),
+        List(
+          Apply(
+            Select(Ident(TermName("Columns")), TermName("apply")),
+            columnsSelector
+          ),
+          whereSelector
+        )
+      )
+    ) = f
 
     val tableFullName = tpt.tpe.toString
     val dotLastIndex = tableFullName.lastIndexOf('.')
     val tableName = tableFullName.substring(dotLastIndex + 1)
 
-    def sqlTemplate(columns: String) = s"SELECT $columns FROM $tableName"
+    def sqlSelectFromTemplate(columns: String) = s"SELECT $columns FROM $tableName"
 
-    val sql = columnsSelector match {
-      case Select(Ident(TermName(_)), TermName(columnName)) =>
-        sqlTemplate(columnName)
-      case Apply(_, values) =>
-        val columns = values.map(tree => {
-          val Select(Ident(TermName(_)), TermName(columnName)) = tree
-          columnName
-        })
+    val columns = columnsSelector.map(colSelector => {
+      val Select(Ident(TermName(_)), TermName(columnName)) = colSelector
+      columnName
+    })
 
-        sqlTemplate(columns.mkString(", "))
+    def whereSelectorToString(tree: c.universe.Tree): String = {
+      val Apply(select, list) = tree
+      (select, list) match {
+        case (
+          Select(Select(Ident(TermName(_)), TermName(col)), TermName(term)),
+          List(Literal(Constant(value)))
+        ) => predicateToString(tree)
+        case (Select(newTree, TermName("$bar$bar")), List(treeInList)) =>
+          whereSelectorToString(newTree) + " OR " + whereSelectorToString(treeInList)
+        case (Select(newTree, TermName("$amp$amp")), List(treeInList)) =>
+          whereSelectorToString(newTree) + " AND " + whereSelectorToString(treeInList)
+      }
     }
 
-//    val Function(List(ValDef(_, _, tpt, EmptyTree)), Select(Ident(TermName(_), TermName(columnName)))) = f
-//
-//    val Function(List(ValDef(_, _, tpt, EmptyTree)), Apply(_, values)) = f
-//    val tableFullName = tpt.tpe.toString
-//    val dotLastIndex = tableFullName.lastIndexOf('.')
-//    val tableName = tableFullName.substring(dotLastIndex + 1)
-//
-//    val columns = values.map(tree => {
-//      val Select(Ident(TermName(_)), TermName(columnName)) = tree
-//      columnName
-//    })
-//
-//    val sql = s"SELECT ${columns.mkString(", ")} FROM $tableName"
+    def predicateToString(tree: c.universe.Tree): String = {
+      val Apply(Select(Select(Ident(TermName(_)), TermName(col)), TermName(term)), List(Literal(Constant(value)))) = tree
+      val terms = term.split("\\$").filter(_.nonEmpty).toList
+      val sign = termsToString(terms)
+      val valueStr = valueToString(value)
+      s"$col $sign $valueStr"
+    }
 
-//    c.Expr(q"${showRaw(tpt)}")
+    def termsToString(terms: List[String]): String = {
+      terms match {
+        case "eq" :: "eq" :: Nil => "="
+        case "eq" :: m => "=" + termsToString(m)
+        case "greater" :: m => ">" + termsToString(m)
+        case "less" :: m => "<" + termsToString(m)
+        case _ => ""
+      }
+    }
+
+    def valueToString(value: Any): String = {
+      if (value.isInstanceOf[String]) s"'$value'"
+      else value.toString
+    }
+
+    val selectFrom = sqlSelectFromTemplate(columns.mkString(", "))
+    val where = "WHERE " + whereSelectorToString(whereSelector)
+    val sql = s"$selectFrom $where"
+
+/*    Function(
+      List(ValDef(Modifiers(PARAM), TermName("t"), TypeTree().setOriginal(Ident(com.github.dedkovva.TABLE_1)), EmptyTree)),
+      Block(
+        List(
+          ValDef(
+            Modifiers(ARTIFACT),
+            TermName("x$1"),
+            TypeTree(),
+            Apply(
+              Select(
+                Apply(
+                  Select(
+                    Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_A")), TermName("$eq$eq")), List(Literal(Constant("col-a-v")))),
+                    TermName("$bar$bar")
+                  ),
+                  List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_B")), TermName("$eq$eq")), List(Literal(Constant("col-b-v0")))))
+                ),
+                TermName("$bar$bar")
+              ),
+              List(
+                Apply(
+                  Select(
+                    Apply(
+                      Select(
+                        Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_B")), TermName("$eq$eq")), List(Literal(Constant("col-b-v1")))),
+                        TermName("$amp$amp")
+                      ),
+                      List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_C")), TermName("$greater")), List(Literal(Constant(0)))))
+                    ),
+                    TermName("$amp$amp")
+                  ),
+                  List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_C")), TermName("$less")), List(Literal(Constant(10)))))
+                )
+              )
+            )
+          ),
+          ValDef(
+            Modifiers(ARTIFACT),
+            TermName("x$2"),
+            TypeTree(),
+            Apply(
+              Select(Ident(com.github.dedkovva.Columns), TermName("apply")),
+              List(Select(Ident(TermName("t")), TermName("COLUMN_A")), Select(Ident(TermName("t")), TermName("COLUMN_B")))
+            )
+          )
+        ),
+        Apply(Select(Ident(com.github.dedkovva.SqlSelect), TermName("apply")), List(Ident(TermName("x$2")), Ident(TermName("x$1"))))
+      )
+    )*/
+
+/*  Function(
+      List(ValDef(Modifiers(PARAM), TermName("t"), TypeTree().setOriginal(Ident(com.github.dedkovva.TABLE_1)), EmptyTree)),
+      Apply(
+        Select(Ident(com.github.dedkovva.SqlSelect), TermName("apply")),
+        List(
+          Apply(
+            Select(Ident(com.github.dedkovva.Columns), TermName("apply")),
+            List(Select(Ident(TermName("t")), TermName("COLUMN_A")), Select(Ident(TermName("t")), TermName("COLUMN_B")))
+          ),
+          Apply(
+            Select(
+              Apply(
+                Select(
+                  Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_A")), TermName("$eq$eq")), List(Literal(Constant("col-a-v")))),
+                  TermName("$bar$bar")
+                ),
+                List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_B")), TermName("$eq$eq")), List(Literal(Constant("col-b-v0")))))
+              ),
+              TermName("$bar$bar")
+            ),
+            List(
+              Apply(
+                Select(
+                  Apply(
+                    Select(
+                      Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_B")), TermName("$eq$eq")), List(Literal(Constant("col-b-v1")))),
+                      TermName("$amp$amp")
+                    ),
+                    List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_C")), TermName("$greater")), List(Literal(Constant(0)))))
+                  ),
+                  TermName("$amp$amp")
+                ),
+                List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_C")), TermName("$less")), List(Literal(Constant(10)))))
+              )
+            )
+          )
+        )
+      )
+    )*/
+
+/*    Function(
+      List(ValDef(Modifiers(PARAM), TermName("t"), TypeTree().setOriginal(Ident(com.github.dedkovva.TABLE_1)), EmptyTree)),
+      Apply(
+        Select(Ident(com.github.dedkovva.SqlSelect), TermName("apply")),
+        List(
+          Apply(
+            Select(Ident(com.github.dedkovva.Columns), TermName("apply")),
+            List(Select(Ident(TermName("t")), TermName("COLUMN_A")), Select(Ident(TermName("t")), TermName("COLUMN_B")))
+          ),
+          Apply(
+            Select(
+              Apply(
+                Select(
+                  Apply(
+                    Select(
+                      Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_A")), TermName("$eq$eq")), List(Literal(Constant("col-a-v")))),
+                      TermName("$bar$bar")
+                    ),
+                    List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_B")), TermName("$eq$eq")), List(Literal(Constant("col-b-v0")))))
+                  ),
+                  TermName("$bar$bar")
+                ),
+                List(
+                  Apply(
+                    Select(
+                      Apply(
+                        Select(
+                          Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_B")), TermName("$eq$eq")), List(Literal(Constant("col-b-v1")))),
+                          TermName("$amp$amp")
+                        ),
+                        List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_C")), TermName("$greater")), List(Literal(Constant(0)))))
+                      ),
+                      TermName("$amp$amp")
+                    ),
+                    List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_C")), TermName("$less$eq")), List(Literal(Constant(10)))))
+                  )
+                )
+              ),
+              TermName("$bar$bar")
+            ),
+            List(Apply(Select(Select(Ident(TermName("t")), TermName("COLUMN_A")), TermName("$eq$eq")), List(Literal(Constant("col-a-v1")))))
+          )
+        )
+      )
+    )*/
+
+    //    c.Expr(q"${showRaw(tpt)}")
 //    c.Expr(q"${showRaw(f)}")
 //    c.Expr(q"$tableName")
     c.Expr(q"$sql")
--- macros-target/src/main/scala/com/github/dedkovva/SqlExample.scala
+++ macros-target/src/main/scala/com/github/dedkovva/SqlExample.scala
@@ -4,18 +4,31 @@ package com.github.dedkovva
   * Created by dedkov-va on 04.03.18.
   */
 object SqlExample extends App {
-//  def sqlSelect(f: _ => _) = select(f)
+  println(Macros.select((t: TABLE_1) => SqlSelect(Columns(t.COLUMN_A), t.COLUMN_A == "col-a-v")))
 
-//  println(Macros.select((t: TABLE_1) => SqlSelect(Columns(t.COLUMN_A), t.COLUMN_A == "col-a-v")))
+//  println(Macros.select((t: TABLE_1) => SqlSelect(
+//    where = t.COLUMN_A == "col-a-v" || t.COLUMN_B == "col-b-v0" ||
+//      t.COLUMN_B == "col-b-v1" && t.COLUMN_C > 0 && t.COLUMN_C < 10,
+//    columns = Columns(t.COLUMN_A, t.COLUMN_B))))
+
+  println(Macros.select((t: TABLE_1) => SqlSelect(
+    Columns(t.COLUMN_A, t.COLUMN_B),
+    t.COLUMN_A == "col-a-v" || t.COLUMN_B == "col-b-v0" ||
+      t.COLUMN_B == "col-b-v1" && t.COLUMN_C > 0 && t.COLUMN_C < 10)))
+
+  println(Macros.select((t: TABLE_1) => SqlSelect(
+    Columns(t.COLUMN_A, t.COLUMN_B),
+    t.COLUMN_A == "col-a-v" || t.COLUMN_B == "col-b-v0" ||
+      t.COLUMN_B == "col-b-v1" && t.COLUMN_C > 0 && t.COLUMN_C <= 10 ||
+      t.COLUMN_A == "col-a-v1")))
+
 //  println(Macros.select((t: TABLE_1) => SqlSelect(
 //    Columns(t.COLUMN_A, t.COLUMN_B),
-//    t.COLUMN_A == "col-a-v" || t.COLUMN_B == "col-b-v")))
-//  println(Macros.select((t: TABLE_1) => SqlSelect(
-//    Columns(t.COLUMN_A, t.COLUMN_B, t.COLUMN_C),
-//    t.COLUMN_A == "col-a-v" && t.COLUMN_C == 1)))
+//    t.COLUMN_A == "col-a-v" || t.COLUMN_B == "col-b-v0" ||
+//      t.COLUMN_B == "col-b-v1" && t.COLUMN_C > 0 && (t.COLUMN_C <= 10 ||
+//        t.COLUMN_A == "col-a-v1"))))
 
-  println(Macros.select((t: TABLE_1) => t.COLUMN_A))
-  println(Macros.select((t: TABLE_1) => (t.COLUMN_A, t.COLUMN_B)))
-  println(Macros.select((t: TABLE_1) => (t.COLUMN_A, t.COLUMN_B, t.COLUMN_C)))
-//  println(Macros.select((t: TABLE_1) => (t.COLUMN_A, t.COLUMN_B, t.COLUMN_C, "1")))
+  println(Macros.select((t: TABLE_1) => SqlSelect(
+    Columns(t.COLUMN_A, t.COLUMN_B, t.COLUMN_C),
+    t.COLUMN_A == "col-a-v" && t.COLUMN_C == 1)))
 }
